6e69aa2005cf58e4463b58ce9a87c2196c865447
KosaJr <kosajunior@gmail.com>
2020-10-03 10:48:22 +0200

    feature(Script/SphereCreator): added sphere creator

---
+++ Assets/Scripts/SphereCreator.cs
@@ -0,0 +1,103 @@
+﻿using GravitySpheres.Scripts.Utilities;
+using UnityEngine;
+
+namespace GravitySpheres.Scripts
+{
+    public class SphereCreator : MonoBehaviour
+    {
+        #region Variables
+
+        [SerializeField] private GravitySpheresSettings settings;
+        [SerializeField] private Transform              spheresPoolTransform;
+
+        private GravitySphere[] spheresPool;
+        private int             nextSphereIndex = 0;
+
+        private float sphereShowDelay = 0f;
+
+        #endregion variables
+
+        #region Constructor & inits
+
+        private void Awake()
+        {
+            if (ValidateReferences())
+                CreateSpheresPool();
+        }
+
+        private bool ValidateReferences()
+        {
+            bool areSettingsSet      = settings;
+            bool arePoolTransformSet = spheresPoolTransform;
+            if (areSettingsSet && arePoolTransformSet)
+                return true;
+
+            if (areSettingsSet == false)
+                Debug.LogError("Settings unavailable!");
+
+            if (arePoolTransformSet == false)
+                Debug.LogError("SpheresPoolTransform unavailable!");
+
+            Debug.LogError("Aborting...");
+            AppUtility.Quit();
+
+            return false;
+        }
+
+        private void CreateSpheresPool()
+        {
+            spheresPool = new GravitySphere[settings.SpheresLimit];
+
+            for (int i = 0; i < spheresPool.Length; i++)
+                spheresPool[i] = CreateSphere();
+        }
+
+        private GravitySphere CreateSphere()
+        {
+            var sphere = Instantiate(settings.SpherePrefab, spheresPoolTransform);
+            sphere.gameObject.SetActive(false);
+
+            return sphere;
+        }
+
+        #endregion constructor & inits
+
+        #region Private methods
+
+        private void Update()
+        {
+            TryShowSphere();
+        }
+
+        private void TryShowSphere()
+        {
+            CalculateSphereShowDelay();
+
+            if (IsTimeToShowSphere() == false)
+                return;
+
+            ShowSphere();
+            ResetSphereShowDelay();
+        }
+
+        private void CalculateSphereShowDelay() => sphereShowDelay += Time.deltaTime;
+
+        private bool IsTimeToShowSphere() => sphereShowDelay >= settings.CreateDelay;
+
+        private void ShowSphere()
+        {
+            spheresPool[nextSphereIndex++].gameObject.SetActive(true);
+
+            if (AreAllSpheresVisible())
+                DisableDisplaying();
+        }
+
+        private bool AreAllSpheresVisible() => nextSphereIndex == spheresPool.Length;
+
+        private void DisableDisplaying() => enabled = false;
+
+        private void ResetSphereShowDelay() => sphereShowDelay = 0f;
+
+        #endregion private methods
+    }
+}
